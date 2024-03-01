ee3c13fc4d9ec2183dd383fb2eefacc17e65af31
Kondraschov Andrew <kondraschov.andrew1999@gmail.com>
2024-03-01 11:39:53 -0500

    create vet page and controller close #6

---
+++ pet-clinic-web/src/main/java/learning/spring/controllers/VetController.java
@@ -0,0 +1,13 @@
+package learning.spring.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class VetController {
+
+    @RequestMapping({"/vets", "vest/index", "vets/index.html"})
+    public String listVets() {
+        return "vets/index";
+    }
+}

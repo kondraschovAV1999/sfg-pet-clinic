745a089d43cb3fd5cf0a958ae056a81af1e4cbf2
Kondraschov Andrew <kondraschov.andrew1999@gmail.com>
2024-03-01 11:24:44 -0500

    add index page and controller. closes #8

---
+++ pet-clinic-web/src/main/java/learning/spring/controllers/indexController.java
@@ -0,0 +1,13 @@
+package learning.spring.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class indexController {
+    @RequestMapping({"", "/", "index.html"})
+    public String index() {
+
+        return "index";
+    }
+}

6cf61e300b92c1916fd8c2dd614ca5d54191ea4e
Kondraschov Andrew <kondraschov.andrew1999@gmail.com>
2024-03-11 15:28:58 -0400

    update vet to use JPA. closes #36

--- pet-clinic-data/src/main/java/learning/spring/model/Speciality.java
+++ pet-clinic-data/src/main/java/learning/spring/model/Speciality.java
@@ -1,7 +1,13 @@
 package learning.spring.model;
 
-public class Speciality extends BaseEntity {
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
 
+@Entity
+@Table(name = "specialities")
+public class Speciality extends BaseEntity {
+    @Column(name = "description")
     private String description;
 
     public String getDescription() {

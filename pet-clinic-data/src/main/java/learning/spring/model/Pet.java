95cf43cfe2ff5b75cc51abbf667cc688f24d1212
Kondraschov Andrew <kondraschov.andrew1999@gmail.com>
2024-03-11 19:26:25 -0400

    add to Pet addVisit method, closes #61

--- pet-clinic-data/src/main/java/learning/spring/model/Pet.java
+++ pet-clinic-data/src/main/java/learning/spring/model/Pet.java
@@ -63,4 +63,9 @@ public class Pet extends BaseEntity {
     public void setVisits(Set<Visit> visits) {
         this.visits = visits;
     }
+
+    public void addVisit(Visit visit){
+        visits.add(visit);
+        visit.setPet(this);
+    }
 }

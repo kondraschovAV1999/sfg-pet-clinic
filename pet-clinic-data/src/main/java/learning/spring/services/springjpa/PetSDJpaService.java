4375893f3dec69b56e9ae68716d1d8820b94feee
Kondraschov Andrew <kondraschov.andrew1999@gmail.com>
2024-03-11 17:02:04 -0400

    create SD JPA Vet speciality service. closes #43

--- pet-clinic-data/src/main/java/learning/spring/services/springjpa/PetSDJpaService.java
+++ pet-clinic-data/src/main/java/learning/spring/services/springjpa/PetSDJpaService.java
@@ -3,10 +3,13 @@ package learning.spring.services.springjpa;
 import learning.spring.model.Pet;
 import learning.spring.repositories.PetRepository;
 import learning.spring.services.PetService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
 
 import java.util.HashSet;
 import java.util.Set;
-
+@Service
+@Profile("springdatajpa")
 public class PetSDJpaService implements PetService {
     private final PetRepository petRepository;
 

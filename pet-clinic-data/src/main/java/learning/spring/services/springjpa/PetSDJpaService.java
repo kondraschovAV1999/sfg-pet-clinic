d2d6876d61002f47972e5a49edc0c43e85d1f7f9
Kondraschov Andrew <kondraschov.andrew1999@gmail.com>
2024-03-11 16:56:06 -0400

    create SD JPA Pet Service. closes #42

---
+++ pet-clinic-data/src/main/java/learning/spring/services/springjpa/PetSDJpaService.java
@@ -0,0 +1,43 @@
+package learning.spring.services.springjpa;
+
+import learning.spring.model.Pet;
+import learning.spring.repositories.PetRepository;
+import learning.spring.services.PetService;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class PetSDJpaService implements PetService {
+    private final PetRepository petRepository;
+
+    public PetSDJpaService(PetRepository petRepository) {
+        this.petRepository = petRepository;
+    }
+
+    @Override
+    public Set<Pet> findAll() {
+        Set<Pet> pets = new HashSet<>();
+        petRepository.findAll().forEach(pets::add);
+        return pets;
+    }
+
+    @Override
+    public Pet findById(Long id) {
+        return petRepository.findById(id).orElse(new Pet());
+    }
+
+    @Override
+    public Pet save(Pet pet) {
+        return petRepository.save(pet);
+    }
+
+    @Override
+    public void delete(Pet pet) {
+        petRepository.delete(pet);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        petRepository.deleteById(id);
+    }
+}
